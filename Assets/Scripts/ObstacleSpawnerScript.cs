292d4ac5210a19f7125c8712e6e1d839521cbb97
David Rice <davidlukerice@gmail.com>
2014-02-20 22:13:42 -0600

    obstacles now spawn

--- Assets/Scripts/ObstacleSpawnerScript.cs
+++ Assets/Scripts/ObstacleSpawnerScript.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class ObstacleSpawnerScript : MonoBehaviour {
 
+	public GameObject[] obstacles;
+	public float spawnDelta = 3.0f;
+	private float timeUntilSpawn = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,18 @@ public class ObstacleSpawnerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.timeUntilSpawn-=Time.deltaTime;
+
+		if (this.timeUntilSpawn <= 0.0f) {
+			this.timeUntilSpawn = this.spawnDelta;
+			SpawnNewObstacle();
+		}
+	}
+
+	private void SpawnNewObstacle() {
+		var index = Mathf.FloorToInt(Random.value*obstacles.Length);
+		var obstacle = (GameObject)Instantiate(obstacles[index]);
+		obstacle.transform.Translate(Vector3.right * 11.0f);
+		obstacle.transform.parent = this.transform;
 	}
 }
